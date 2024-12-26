4b5718d040ccb1bc21308b4af357b2bcef6c9540
web-space-pro <ludmilamedvedeva@MacBook-Pro-Ludmila.local>
2024-12-26 14:18:44 +0200

    acf footer

--- footer.php
+++ footer.php
@@ -11,14 +11,23 @@
 
 ?>
     </main>
+
+<?php
+if (function_exists('get_field')) {
+    $copyright_head = get_field('op_copyright_head', 'options');
+    $copyright = get_field('op_copyright_footer', 'options');
+    $develop = get_field('op_develop_footer', 'options');
+    $developLink = get_field('op_developlink_footer', 'options');
+}
+?>
 	<footer class="site-footer my-6">
         <div class="bg-black-400 rounded-2xl mx-4 md:mx-10 py-4 md:py-6">
             <div class="container">
                 <div class="flex flex-wrap items-center">
                     <div class="xs:w-5/12 md:w-5/12 xs:order-none order-2 w-full">
                         <div class="md:text-sm md:pr-5 xs:py-0 py-3 pr-0 text-gray-500 text-xs font-normal leading-tight">
-                            <p class="text-white-900">©️ Все права защищены. 2024–2025</p>
-                            Представленная на сайте информация не является офертой, вся информация указанная на сайте является рекламой. Подробности уточняйте по номеру +7918009 92 97
+                            <p class="text-white-900"><?=$copyright_head?></p>
+                            <?=$copyright?>
                         </div>
                     </div>
                     <div class="xs:w-7/12 md:w-5/12 xs:order-none order-1 w-full">
@@ -28,8 +37,8 @@
                     </div>
                     <div class="md:w-2/12 xs:order-none order-3 w-full">
                         <div class="text-white-900 text-sm font-normal leading-tight text-center md:text-right">
-                            <p>Разработано в</p>
-                            <a class="text-accent underline hover:no-underline" href="#" target="_blank">wep-space.pro</a>
+                            <h4> <?=$develop?></h4>
+                            <a class="text-accent underline hover:no-underline" href="<?=$developLink['url']?>" target="_blank"><?=$developLink['title']?></a>
                         </div>
                     </div>
                 </div>
--- page.php
+++ page.php
@@ -15,24 +15,49 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<?php if (function_exists(have_posts()) ): ?>
+        <?php while ( have_posts() ) : the_post() ; ?>
+            <?php if ( have_rows('components' )  ) :
+                while( have_rows('components') )
+                {
+                    the_row();
+                    $layout = get_row_layout();
+                    $inclusion = get_stylesheet_directory() . DIRECTORY_SEPARATOR . "partails" . DIRECTORY_SEPARATOR ."tpl-{$layout}.php";
+                    if( file_exists( $inclusion ) )
+                    {
+                        include( $inclusion );
+                    }
+                }
+            else: ?>
+                <section class="componentInfo">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="componentInfo__box">
+                                    <p>Вы не создали не одного блока! Перейдите в редактор страницы для добавление нового блока!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            <?php endif;
+        endwhile; ?>
+<?php  else: ?>
+        <?php
+        while ( have_posts() ) :
+            the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+            get_template_part( 'template-parts/content', 'page' );
 
-			get_template_part( 'template-parts/content', 'page' );
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+        endwhile; // End of the loop.
+        ?>
 
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
+<?php endif; ?>
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
